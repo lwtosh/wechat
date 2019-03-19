3d5f660c8696568ca37e78154f5f7f8e74a82a69
binfei <1615077717@qq.com>
2019-03-19 17:19:21 +0800

    提交

--- pages/demo/demo01.js
+++ pages/demo/demo01.js
@@ -1,14 +1,14 @@
-// pages/demo/demo01.js
-// var wxTimer = require('../../utils/tw.js')
-// var app = getApp();
+
 let timeId;
 let time1;
 let time2;
 
 Page({
   data: {
-    e: '0.000',
+    e: '00:000',
     operateBtn: "开始",
+    showView: false,
+    flag: true,
     imgUrls: [
       '../../asset/iphone-4.jpg',
       '../../asset/ipoone1.png',
@@ -16,10 +16,10 @@ Page({
     ],
     indicatorDots: false,
     autoplay: true,
-    interval: 5000,
+    interval: 3000,
     duration: 1000,
     circular: true,
-    num:0,
+    num: 0,
   },
   changeIndicatorDots(e) {
     this.setData({
@@ -42,60 +42,51 @@ Page({
     })
   },
   Btn: function() {
-    if (!timeId){
+    if (!timeId) {
       this.setData({
         operateBtn: "停止"
       })
       time1 = new Date();
-      timeId = setInterval(function () {
+      timeId = setInterval(function() {
         this.setData({
           e: ((new Date() - time1) / 1000).toFixed(3)
         })
       }.bind(this), 1)
-    }else{
+    } else {
       clearInterval(timeId);
       timeId = null;
       this.setData({
         operateBtn: "再来一局"
       })
       console.log(this.data.e)
-      if(this.data.e===10){
+      if (this.data.e === 10) {
         this.setData({
           e: 10.001
         })
-      }else{
+      } else {
+        this.setData({ flag: true })
         console.log('失败')
       }
     }
   },
+  onChangeShowState: function() {
+    var that = this;
+    that.setData({
+      showView: (!that.data.showView)
+    })
+  },
+  conceal: function() {
+    this.setData({
+      showView: false
+    })
+  },
+  onChangeHideState:function(){
+    this.setData({
+      flag: true
+    })
+  },
   onLoad: function(options) {
-
-  },
-  onReady: function() {
-
-  },
-
-  onShow: function() {
-
-  },
-
-  onHide: function() {
-
-  },
-
-  onUnload: function() {
-
-  },
-
-  onPullDownRefresh: function() {
-
-  },
-
-  onReachBottom: function() {
-
-  },
-
-  onShareAppMessage: function() {
-
+    showView: (options.showView == "true" ? true : false);
+    // flag: (options.flag == "true" ? true : false)
   }
 })
